5eb3f358043911f529076701bbe2a1eb2b21bf0c
raincoat0503 <raincoat.503@gmail.com>
2022-01-10 07:06:04 +0900

    firstcommit

---
+++ module/footer.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+    <img class="footer__logo"></img>
+    <div class="footer__menulist">
+        <ul class="menulist">
+            <li class="menulist__item"><a class="menulist__link" href="#"></a></li>
+            <li class="menulist__item"><a class="menulist__link" href="#"></a></li>
+            <li class="menulist__item"><a class="menulist__link" href="#"></a></li>
+        </ul>
+    </div>
+    <div class="footer__medialist">
+        <ul class="medialist">
+            <li class="medialist__item"><img class="medialist__icon" src="../media/img/socialmedia/twitter.svg" alt=""></li>
+            <li class="medialist__item"><img class="medialist__icon" src="../media/img/socialmedia/facebook.svg" alt=""></li>
+            <li class="medialist__item"><img class="medialist__icon" src="../media/img/socialmedia/behance.svg" alt=""></li>
+            <li class="medialist__item"><img class="medialist__icon" src="../media/img/socialmedia/pinterest.svg" alt=""></li>
+            <li class="medialist__item"><img class="medialist__icon" src="../media/img/socialmedia/dribbble.svg" alt=""></li>
+        </ul>
+    </div>
+    <p class="footer__copyright">Copyright &copy; 2022 MAKINOKAITO All Rights Reserved.</p>
+</footer>
---
+++ module/header.php
@@ -0,0 +1,31 @@
+<header class="header">
+    <ul class="menu menu--pc">
+        <li class="menu__item">HOME</li>
+        <hr class="menu__split" noshade>
+        <li class="menu__item">PRODUCTS</li>
+        <hr class="menu__split" noshade>
+        <li class="menu__item">ABOUT</li>
+        <hr class="menu__split" noshade>
+        <li class="menu__item">CONTACT</li>
+    </ul>
+    <img src="../media/img/module/logo.png" alt="" class="logo">
+    <div class="rightitems">
+        <form class="search" action="http://www.google.co.jp/search" method="get">
+            <input type="text" class="search__text">
+            <input type="image" class="search__submit" src="../media/img/module/search.svg">
+        </form>
+        <a class="rightitems__loginbtn">Login</a>
+        <a class="rightitems__signinbtn">Signin</a>
+    </div>
+</header>
+<div class="secondheader">
+    <ul class="menu menu--tab menu--sp">
+        <li class="menu__item">HOME</li>
+        <hr class="menu__split" noshade>
+        <li class="menu__item">PRODUCTS</li>
+        <hr class="menu__split" noshade>
+        <li class="menu__item">ABOUT</li>
+        <hr class="menu__split" noshade>
+        <li class="menu__item">CONTACT</li>
+    </ul>
+</div>
